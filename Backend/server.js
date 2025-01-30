3947cdcfbd4ed0d11d134b2b504fbc78bb803320
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-01-30 23:33:59 +0530

    final touch

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,8 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-// Middleware
 app.use(cors());
-app.use(express.json());  // To parse JSON requests
+app.use(express.json());  
 
 // Routes
 app.use('/api/users', userRoutes);
